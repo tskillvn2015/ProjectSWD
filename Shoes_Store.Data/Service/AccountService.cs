58a73da12042dfc43c4eca2b9637b511d3185097
QuangTPSE63422 <ziodyne123@gmail.com>
2020-03-11 16:39:16 +0700

    fsf

--- Shoes_Store.Data/Service/AccountService.cs
+++ Shoes_Store.Data/Service/AccountService.cs
@@ -33,7 +33,7 @@ namespace Shoes_Store.Data.Service
             var result = _unitOfWork.AccountRepository.Get(c => c.Username.Equals(model.Username));
             if(result.FirstOrDefault() != null)
             {
-                throw new Exception("This username already exist!");
+                throw new Exception("This username already exist");
             }
             var account = new Account
             {
