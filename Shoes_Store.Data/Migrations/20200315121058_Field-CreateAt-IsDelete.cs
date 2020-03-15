7d631c51a41f3df8243fef124d031aa32c1556cc
Tan <Tskillvn2015@yahoo.com.vn>
2020-03-15 19:15:05 +0700

    add field for entites and config for Cors

---
+++ Shoes_Store.Data/Migrations/20200315121058_Field-CreateAt-IsDelete.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shoes_Store.Data.Migrations
+{
+    public partial class FieldCreateAtIsDelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Product",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDelete",
+                table: "Product",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "OrderDetail",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDelete",
+                table: "OrderDetail",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Order",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDelete",
+                table: "Order",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Account",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDelete",
+                table: "Account",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "IsDelete",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "OrderDetail");
+
+            migrationBuilder.DropColumn(
+                name: "IsDelete",
+                table: "OrderDetail");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Order");
+
+            migrationBuilder.DropColumn(
+                name: "IsDelete",
+                table: "Order");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Account");
+
+            migrationBuilder.DropColumn(
+                name: "IsDelete",
+                table: "Account");
+        }
+    }
+}
