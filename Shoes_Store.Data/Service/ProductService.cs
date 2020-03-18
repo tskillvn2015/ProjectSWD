facc4a22bad18c7cc2644bc046bcc11d787e2996
sonic2209 <51818304+sonic2209@users.noreply.github.com>
2020-03-18 12:35:16 +0700

    fix CreateProduct

--- Shoes_Store.Data/Service/ProductService.cs
+++ Shoes_Store.Data/Service/ProductService.cs
@@ -6,17 +6,21 @@ using Shoes_Store.Data.Entities;
 using Shoes_Store.Data.Interfaces;
 using Shoes_Store.Data.EF;
 using Shoes_Store.Data.ViewModels;
+using Shoes_Store.Common;
+using Shoes_Store.Interfaces;
 
 namespace Shoes_Store.Data.Service
 {
     public class ProductService : IProductService
     {
-        private readonly ShoeserDbContext _context;
-        public ProductService(ShoeserDbContext context)
+        private readonly IApiResponse _apiResponse;
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductService(IUnitOfWork unitOfWork,IApiResponse apiResponse)
         {
-            _context = context;
+            _unitOfWork = unitOfWork;
+            _apiResponse = apiResponse;
         }
-        public async Task<int> CreateProduct(CreateProductViewModel model)
+        public async Task<Object> CreateProduct(CreateProductViewModel model)
         {
             Product product = new Product();
             product.Name = model.Name;
@@ -26,8 +30,10 @@ namespace Shoes_Store.Data.Service
             product.Description = model.Description;
             product.Quantity = model.Quantity;
             product.Status = model.Status;
-            _context.Products.Add(product);
-            return await _context.SaveChangesAsync();
+            product.CreatedAt = DateTime.Now;
+            _unitOfWork.ProductRepository.Add(product);
+            var result = _apiResponse.Ok(_unitOfWork.Save());
+            return result;
         }
     }
 }
