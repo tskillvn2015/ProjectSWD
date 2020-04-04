81c7579c8afd22452d697c98be86ac3c6eb93556
Tan <Tskillvn2015@yahoo.com.vn>
2020-04-04 18:08:47 +0700

    add seeding data account sa

--- Shoes_Store.Data/Migrations/20200404110719_SeedData.Designer.cs
+++ Shoes_Store.Data/Migrations/20200404110719_SeedData.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Shoes_Store.Data.EF;
 
 namespace Shoes_Store.Data.Migrations
 {
     [DbContext(typeof(ShoeserDbContext))]
-    partial class ShoeserDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200404110719_SeedData")]
+    partial class SeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
